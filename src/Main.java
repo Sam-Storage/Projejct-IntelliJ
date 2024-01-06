cc701b1771fe2e838ef758c4b340226bc7241665
samgrigorenko <samiam41028@gmail.com>
2024-01-05 21:52:06 -0800

    Made a few changes to the main class.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+/**
+ *
+ * @author Sam
+ * @version 2.0
+ */
 public class Main {
+    /**
+     * @param arg this is the main file
+     */
     public static void main(String[] arg){
         System.out.println("Hello world");
     }
